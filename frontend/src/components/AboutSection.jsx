4b9950c133528d6fa008cf99edb93f4bbd0933c0
Arya N M <aryasathy999@gmail.com>
2026-07-12 10:40:52 +0530

    first commit

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -14,11 +14,11 @@ export default function AboutSection({ showButton = true }) {
           <div className="about-one_image-column col-lg-6 col-md-12 col-sm-12">
             <div className="about-one_image-outer">
               <div className="about-one_image">
-                <img src={aboutImg} alt="Interior flooring showcase" />
+                <img src={aboutImg} alt="Premium tiles &amp; sanitary ware showcase" />
               </div>
               <div className="about-one_experiance">
-                <div className="about-one_circle" style={{ backgroundImage: `url(${experienceIcon})` }} />
-                <span style={{color: "#ffffff"}}>15</span>
+                {/* <div className="about-one_circle" style={{ backgroundImage: `url(${experienceIcon})` }} /> */}
+                {/* <span style={{color: "#ffffff"}}>KAG</span> */}
               </div>
             </div>
           </div>
@@ -27,9 +27,9 @@ export default function AboutSection({ showButton = true }) {
             <div className="about-one_content-outer">
               <div className="sec-title">
                 <div className="sec-title_title"><i className="flaticon-wood-1" /> About us</div>
-                <h2 className="sec-title_heading">Best Flooring &amp; Tiling Agency</h2>
+                <h2 className="sec-title_heading">Premium Tiles, Sanitarywares &amp; Bath Fittings</h2>
                 <div className="sec-title_text">
-                  This esteemed level of status is only possible by the consistent implementation of the highest possible standards of service
+                  We are your one-stop destination for premium tiles, sanitary wares, and luxurious bath fittings, offering high-quality products directly from KAG's exclusive collections.
                 </div>
               </div>
 
@@ -37,39 +37,39 @@ export default function AboutSection({ showButton = true }) {
                 <div className="about-one_feature col-lg-6 col-md-6 col-sm-12">
                   <div className="about-one_feature-inner">
                     <div className="about-one_feature-icon flaticon-paving" />
-                    Quality Flooring Services
+                    Genuine Tiles Collections
                   </div>
                 </div>
                 <div className="about-one_feature col-lg-6 col-md-6 col-sm-12">
                   <div className="about-one_feature-inner">
                     <div className="about-one_feature-icon flaticon-tile" />
-                    No1 Flooring <br /> Services
+                    Premium Sanitaryware &amp; <br /> Bath Fittings
                   </div>
                 </div>
               </div>
 
               <div className="about-one_text">
-                We are a small Kent based company with over 15 years experience and have built a great reputation from our previous customers.
+                We are a premier startup showroom in Chennai and a proud authorized channel partner of KAG, a brand renowned for over 25 years of industry-leading excellence. We specialize in supplying high-quality floor and wall tiles as our primary offering, along with premium sanitary wares, elegant bath fittings, and other building ceramics directly from KAG's trusted catalog.
               </div>
 
               <div className="row clearfix">
                 <div className="column col-lg-6 col-md-6 col-sm-12">
                   <ul className="about-one_list">
-                    <li><i className="flaticon-checked-1" />Helpful staff</li>
-                    <li><i className="flaticon-checked-1" />Community involvement</li>
+                    <li><i className="flaticon-checked-1" />Wide Range of KAG Designs</li>
+                    <li><i className="flaticon-checked-1" />Premium Sanitary &amp; Fittings</li>
                   </ul>
                 </div>
                 <div className="column col-lg-6 col-md-6 col-sm-12">
                   <ul className="about-one_list">
-                    <li><i className="flaticon-checked-1" />Family-owned &amp; operated</li>
-                    <li><i className="flaticon-checked-1" />Excellent relationships</li>
+                    <li><i className="flaticon-checked-1" />Direct Channel Pricing</li>
+                    <li><i className="flaticon-checked-1" />Expert Product Guidance</li>
                   </ul>
                 </div>
               </div>
 
               {showButton && (
                 <div className="about-one_button">
-                  <Link to="/about" className="theme-btn btn-style-one">
+                  <Link to="/about" className="theme-btn btn-style-one" style={{ borderRadius: '4px', padding: '16px 22px' }}>
                     <span className="btn-wrap">
                       <span className="text-one">More About us</span>
                       <span className="text-two">More About us</span>
